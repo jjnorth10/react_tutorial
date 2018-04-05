e271c9dd0fbed2e2cbee00d350f904c4c998d9da
jjnorth10 <jjnorthover@gmail.com>
2018-04-05 08:27:04 -0500

    Conditional Rendering

--- src/index.js
+++ src/index.js
@@ -6,22 +6,53 @@ import registerServiceWorker from './registerServiceWorker';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+
+    const element1 = (<h1>Hello New User</h1>);
+    const element2 = (<h1>Welcome Back</h1>);
+    return this.props.isGuest ? element1 : element2;
+    /*(
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );*/
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
 }
 
-function tick() {
-  ReactDOM.render(
-    <Clock date={new Date()} />,
-    document.getElementById('root')
-  );
-}
+ReactDOM.render(
+  <Clock isGuest={false} />,
+  document.getElementById('root')
+);
 
-setInterval(tick, 1000);
+
+
+//setInterval(tick, 1000);
 
 registerServiceWorker();
