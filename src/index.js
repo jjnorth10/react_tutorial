9835bd06bdc8946edddca27082bf6ae4fa537a25
jjnorth10 <jjnorthover@gmail.com>
2018-04-25 10:16:28 -0500

    Started temperature component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 /*class Clock extends React.Component {
   constructor(props) {
     super(props);
