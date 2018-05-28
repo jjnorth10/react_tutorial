fcf4039fba3aaad1c4eb7495e514e9666af1fcb2
Jordan Northover <jjnorthover@gmail.com>
2018-05-27 22:47:47 -0500

    Implemented unit testing libraries

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import { expect } from 'chai';
+import Projects from './Components/Projects';
 import App from './App';
+
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App component testing', function() {
+  it('renders welcome message', function() {
+    const wrapper = shallow(<App />);
+    const welcome = <h1 className='App-title'>Welcome to React</h1>;
+    expect(wrapper.contains(welcome)).to.equal(true);
+  });
+
+  it('renders project list', function(){
+    const wrapper = shallow(<App />);
+    const projects = <Projects />;
+
+  });
 });
